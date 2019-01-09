6c5086a5b2053a434fdde41715bcc42b8242a457
Rajat Mittal <rjmittal97@gmail.com>
2019-01-09 10:08:38 +0530

    Initial commit

---
+++ app/src/main/java/com/example/elf/constraint_layout/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.elf.constraint_layout
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.support.design.widget.TabLayout
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+
+
+    }
+}
+
